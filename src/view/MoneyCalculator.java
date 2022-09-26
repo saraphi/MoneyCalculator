bfabc232555c4dfb28b678a2bf68a1e90a8023f3
Sara González Ramírez <saragonza.lez0608@gmail.com>
2022-09-26 18:55:45 +0100

    Cuerpo GUI idea básica

--- src/view/MoneyCalculator.java
+++ src/view/MoneyCalculator.java
@@ -1,5 +1,25 @@
 package view;
 
-public class MoneyCalculator {
+import model.*;
+import javax.swing.*;
 
+public class MoneyCalculator extends JPanel {
+
+    private JComboBox cToCombo;
+    private JComboBox cFromCombo;
+    private JTextField moneyToText;
+    private JTextField moneyFromText;
+    
+    private Currency cTo;
+    private Currency cFrom;
+    private Money moneyTo;
+    private Money moneyFrom;
+
+    public MoneyCalculator() {
+        
+    }
+    
+    public void refresh() {
+        
+    }
 }
